5d6a4a3054981273fc7d011b7e5652d53266d66c
Aashrey <aashrey@appfoster.com>
2024-04-09 15:37:19 +0530

    changes

--- task-13-laravel/src/myapp/resources/views/layouts/app.blade.php
+++ task-13-laravel/src/myapp/resources/views/layouts/app.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel CRUD</title>
+    <title>Laravel</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Laravel CRUD</a>
+            <a class="navbar-brand" href="#">Laravel</a>
         </div>
     </nav>
 
