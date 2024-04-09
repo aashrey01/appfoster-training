5835a98fafb83cea9b77ebbc76b3801a67e87255
Aashrey <aashrey@appfoster.com>
2024-04-09 15:39:50 +0530

    changes

--- task-13-laravel/src/myapp/resources/views/layouts/app.blade.php
+++ task-13-laravel/src/myapp/resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Laravel</a>
         </div>
     </nav>
 
